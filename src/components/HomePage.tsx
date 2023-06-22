89fbcfafef490b92d54f344554c2b972afff642c
devlak2001 <vladimirkaric2001@gmail.com>
2023-06-22 20:01:00 +0200

    working on home page part3, added share functionality, added og meta tags

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,11 +1,12 @@
 import "../styles/homePage.scss";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 const bkgMusic = new Audio(
   process.env.PUBLIC_URL + "/assets/audios/bkgMusic.mp3"
 );
 export default function HomePage() {
   const [speakerOn, setSpeakerOn] = useState(false);
+  const phoneVideo = useRef<HTMLVideoElement>(null);
   useEffect(() => {}, []);
   return (
     <>
@@ -123,42 +124,42 @@ export default function HomePage() {
             xmlns="http://www.w3.org/2000/svg"
             className={`speakerWaves ${speakerOn ? "on" : ""}`}
           >
-            <g clip-path="url(#clip0_298_126)">
+            <g clipPath="url(#clip0_298_126)">
               <path
                 className="first"
                 d="M10.2701 4.79959L77.9326 35.3874L65.741 52.898L111.656 84.9766L47.4484 56.3745L58.3349 38.3877L10.2701 4.79959Z"
                 fill="black"
                 stroke="white"
-                stroke-width="1.22105"
-                stroke-miterlimit="2"
-                stroke-linejoin="round"
+                strokeWidth="1.22105"
+                strokeMiterlimit="2"
+                strokeLinejoin="round"
               />
               <path
                 className="second"
                 d="M5.23938 111.473L62.0303 91.6745L64.6432 108.758L109.498 101.971L55.8442 121.005L52.1994 104.372L5.23938 111.473Z"
                 fill="black"
                 stroke="white"
-                stroke-width="1.22105"
-                stroke-miterlimit="2"
-                stroke-linejoin="round"
+                strokeWidth="1.22105"
+                strokeMiterlimit="2"
+                strokeLinejoin="round"
               />
               <path
                 className="third"
                 d="M43.4502 294.027L94.8897 268.655L85.0218 255.454L119.633 229.495L70.8106 253.246L79.6822 266.847L43.4502 294.027Z"
                 fill="black"
                 stroke="white"
-                stroke-width="1.22105"
-                stroke-miterlimit="2"
-                stroke-linejoin="round"
+                strokeWidth="1.22105"
+                strokeMiterlimit="2"
+                strokeLinejoin="round"
               />
               <path
                 className="fourth"
                 d="M35.642 210.298L80.6647 221.747L81.5369 208.427L116.508 210.66L73.949 199.588L72.2524 212.632L35.642 210.298Z"
                 fill="black"
                 stroke="white"
-                stroke-width="1.22105"
-                stroke-miterlimit="2"
-                stroke-linejoin="round"
+                strokeWidth="1.22105"
+                strokeMiterlimit="2"
+                strokeLinejoin="round"
               />
             </g>
             <defs>
@@ -180,7 +181,29 @@ export default function HomePage() {
             alt=""
             className="phoneShadow"
           />
+          {/* <video
+            loop={true}
+            ref={phoneVideo}
+            src={process.env.PUBLIC_URL + "/assets/videos/homePageVideo.mp4"}
+          ></video> */}
           <img
+            // onClick={() => {
+            //   // if (phoneVideo.current!.paused) {
+            //   //   phoneVideo.current!.play();
+            //   // } else {
+            //   //   phoneVideo.current!.pause();
+            //   // }
+            // }}
+            onClick={async () => {
+              const shareData = {
+                url: window.location.href,
+              };
+              try {
+                await navigator.share(shareData);
+              } catch (err) {
+                console.log(err);
+              }
+            }}
             src={process.env.PUBLIC_URL + "/assets/images/HomePage/phone.png"}
             alt=""
             className="phone"
