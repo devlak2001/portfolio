fcfef96f13e479a1c54f1584826918643b84ade7
devlak2001 <vladimirkaric2001@gmail.com>
2023-06-30 19:34:42 +0200

    added sakura leaves falling, some other fixes considering elements size

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,15 @@ import AboutMe from "./components/AboutMe";
 import HomePage from "./components/HomePage";
 import NavBar from "./components/NavBar";
 
-import { useRef } from "react";
+import "./styles/sakura.scss";
+
+import Sakura from "./scripts/sakura";
+
+import { useRef, useEffect } from "react";
 function App() {
+  useEffect(() => {
+    const sakura = new Sakura(".App");
+  }, []);
   return (
     <div className="App">
       <NavBar />
