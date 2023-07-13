9d8f9812073744c2715ee5e49d7c34f5d04fc707
devlak2001 <vladimirkaric2001@gmail.com>
2023-07-13 17:55:42 +0200

    setting up fetch from json

--- src/components/ProjectsPage.tsx
+++ src/components/ProjectsPage.tsx
@@ -1,7 +1,15 @@
 import "../styles/projectsPage.scss";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, useMemo } from "react";
+
+import json from "./projects.json";
 
 export default function ProjectsPage() {
+  const [currentProject, setCurrentProject] = useState<number>(1);
+  // const [jsonData, setJsonData] = useState(json);
+  const project = useMemo(async () => {
+    return json.projects[currentProject - 1];
+  }, []);
+  console.log(project);
   return (
     <>
       <div className="ProjectsPage"></div>
