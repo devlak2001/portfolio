9b56546585ccc0d626ade273263e20b7819d4282
devlak2001 <vladimirkaric2001@gmail.com>
2024-02-06 00:25:45 +0100

    fix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,7 +1,7 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  pathPrefix: "/vladimir_karic",
+  pathPrefix: "/portfolio",
   siteMetadata: {
     title: `Portfolio`,
     siteUrl: `https://www.yourdomain.tld`,
